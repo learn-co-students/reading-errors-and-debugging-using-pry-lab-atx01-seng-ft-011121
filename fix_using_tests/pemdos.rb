f4d6bc51550e5ffbfbc6885abcdf30315e65a70c
Scott Gloyna <scott.gloyna01@gmail.com>
2020-12-21 04:45:23 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,15 @@
 # don't forget to add: require 'pry'
 
+
 def snake_it_up(string)
+
   if string[0] == "s"
-    10 * "s" + string
+
+    10.times {string.prepend("s")}
   else
+
     string
   end
+  string
 end
+
